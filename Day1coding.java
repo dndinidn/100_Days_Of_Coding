24833ff34aa0bafa922824626c08528b654f9fa8
dndinidn <andiniputriamrullahgmail.com>
2023-10-09 16:03:40 +0700

    first commit

---
+++ Day1coding.java
@@ -0,0 +1,19 @@
+
+package day1coding;
+
+public class Day1coding {
+
+    public static void main(String[] args) {
+ 
+String nama = "Dini"; // tipe data untuk teks/kata
+int umur = 19; // tipe data untuk bilangan bulat
+double bb= 56.8; // tipe data untuk angka desimal
+System.out.println("Data diri");
+        System.out.println("nama: "+nama);
+        System.out.println("umur: "+umur);
+        System.out.println("berat badan: "+bb);
+
+        
+    }
+    
+}
