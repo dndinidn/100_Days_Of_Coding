ce0b00902c48b149e4ce2bcb6c2c24cef159bf14
dndinidn <146814774+dndinidn@users.noreply.github.com>
2023-10-11 23:25:37 +0700

    Day3.java

--- Day3.java
+++ Day3.java
@@ -5,7 +5,7 @@ import java.util.Scanner;
 
 public class Day3 {
     public static void main(String[] args) {
-        System.out.print("kalkulator sesat");
+        System.out.println("kalkulator sesat");
         Scanner dn = new Scanner (System.in);
         System.out.print("masukkan angka pertama= ");
         int angkaPertama = dn.nextInt();
