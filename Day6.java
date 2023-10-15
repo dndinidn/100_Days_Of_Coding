18d89b5d888fa5147bc97b84095e8e60a100702c
dndinidn <andiniputriamrullahgmail.com>
2023-10-14 21:56:35 -0700

    tes

---
+++ Day6.java
@@ -0,0 +1,20 @@
+package day.pkg6;
+
+import java.util.Scanner;
+
+public class Day6 {
+
+    public static void main(String[] args) {
+        Scanner dn = new Scanner(System.in);
+        System.out.print("masukkan anngka= ");
+        int BatasAngka = dn.nextInt();
+        for (int i = 0; i < BatasAngka; i++) {
+            if (i % 2 == 1) {
+                System.out.println(i);
+
+            }
+
+        }
+
+    }
+}
