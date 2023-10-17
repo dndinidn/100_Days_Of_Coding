af51a8b7d54814c72fd39da4901649290a526f27
dndinidn <andiniputriamrullah@gmail.com>
2023-10-17 01:45:37 -1200

    Konversi String ke tipe data primitif (int)

---
+++ Day9.java
@@ -0,0 +1,16 @@
+
+package day.pkg9;
+
+public class Day9 {
+
+    public static void main(String[] args) {
+        System.out.println("Konversi String ke tipe data primitif");
+        System.out.println("String ke int");
+        String before = "20";
+        int after = Integer.parseInt(before);
+        System.out.println("sebelum di konversi 50 + "+before +"="+50+before);
+        System.out.println("sesudah di konversi 50 + "+after +"="+(50+after));
+        
+    }
+    
+}
