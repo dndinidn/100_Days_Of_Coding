8ce46cb387738222320fe6963a4682c70aa73963
dndinidn <andiniputriamrullahgmail.com>
2023-10-20 00:24:22 -0700

    Operator Perbandingan

---
+++ Day11.java
@@ -0,0 +1,35 @@
+package day.pkg11;
+
+import java.util.Scanner;
+
+public class Day11 {
+
+    public static void main(String[] args) {
+        System.out.println("Operator Perbandingan");
+        Scanner dn = new Scanner(System.in);
+        System.out.print("masukkan nilai a = ");
+        int a = dn.nextInt();
+        System.out.print("masukkan nilai b = ");
+        int b = dn.nextInt();
+        boolean hasil;
+
+        hasil = a > b; //lebih besat
+        System.out.println(a + ">" + b + " = " + hasil);
+
+        hasil = a < b; //lebih kecil
+        System.out.println(a + "<" + b + " = " + hasil);
+
+        hasil = a >= b; //lebih besar sama dengan
+        System.out.println(a + ">=" + b + " = " + hasil);
+
+        hasil = a <= b; //lebih kecil sama dengan
+        System.out.println(a + "<=" + b + " = " + hasil);
+
+        hasil = a == b; //sama dengan
+        System.out.println(a + "==" + b + " = " + hasil);
+
+        hasil = a != b; //tidak sama dengan
+        System.out.println(a + "!=" + b + " = " + hasil);
+    }
+
+}
