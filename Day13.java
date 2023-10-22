38a6a842e39476294a469d84ca2bf11b784d85b2
dndinidn <andiniputriamrullahgmail.com>
2023-10-22 00:38:39 -0700

    Perulangan (For Each)

---
+++ Day13.java
@@ -0,0 +1,16 @@
+package day.pkg13;
+
+import java.util.Scanner;
+
+public class Day13 {
+
+    public static void main(String[] args) {
+        //perulangan for each
+        String [] unsur = 
+        {"Helium","Neon","Argon","Kripton","Xenon","Radon"};
+        for(String namaUnsur:unsur){
+            System.out.println(namaUnsur);
+        }
+
+    }}
+
