5121885d2d5a1e7d8ad7b7ae0d49b8c81ed7fb32
dndinidn <andiniputriamrullahgmail.com>
2023-10-22 00:51:20 -0700

    Perulangan (do while)

---
+++ Day14.java
@@ -0,0 +1,14 @@
+package day1.pkg4;
+
+public class Day14 {
+
+    public static void main(String[] args) {
+        int latihan = 4;
+        //perulangan do while
+        do {
+            System.out.println(latihan * 2+"");
+            latihan++;
+        }  
+        while(latihan <= 10);
+    }
+}
