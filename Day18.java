9a347d986ebc339989d925cc95e06d4a2c0d8195
dndinidn <andiniputriamrullah>
2023-10-26 15:59:01 +0800

    Stack

---
+++ Day18.java
@@ -0,0 +1,16 @@
+
+package day18;
+
+import java.util.Stack;
+
+public class Day18 {
+//Stack
+    public static void main(String[] args) {
+    Stack<String> tes = new Stack<>();
+        tes.push("alkana");
+        tes.push("alkena");
+        tes.push("alkuna");
+        System.out.println(tes);
+    }
+    
+}
