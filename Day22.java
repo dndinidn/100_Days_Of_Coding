eafc198a34d910fb2f28cc9ff549160c92f3ca18
dndinidn <andiniputriamrullah>
2023-10-31 11:07:15 +0800

    operator Aritmatika dan input

---
+++ Day22.java
@@ -0,0 +1,19 @@
+package day;
+
+import java.util.Scanner;
+
+public class Day22 {
+
+    public static void main(String[] args) {
+        Scanner dn = new Scanner(System.in);
+        System.out.print("masukkan gaji anda= ");
+        int gaji = dn.nextInt();
+        System.out.print("lama lembur= ");
+        int lembur = dn.nextInt();
+        int gajiLembur = 55000;
+        int Totalgaji = gaji + (lembur * gajiLembur);
+        System.out.println("total gaji= " + Totalgaji);
+
+    }
+
+}
