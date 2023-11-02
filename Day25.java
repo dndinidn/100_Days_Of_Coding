37bd78f0b9e1b239e93410476d90095b0fb6fb25
dndinidn <andiniputriamrullahgmail.com>
2023-11-02 03:08:56 -0700

    perulangan

---
+++ Day25.java
@@ -0,0 +1,21 @@
+package day;
+
+import java.util.Scanner;
+
+public class Day25 {
+
+    public static void main(String[] args) {
+        while (true) {
+            Scanner dn = new Scanner(System.in);
+            System.out.print("masukkan nilai a= ");
+            int a = dn.nextInt();
+            System.out.print("masukkan nilai b= ");
+            int b = dn.nextInt();
+            for (int i = 1; i <= 10; i++) {
+                System.out.println(i);
+            }
+
+        }
+    }
+
+}
