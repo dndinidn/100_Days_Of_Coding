5cf92903297de22c6045ee489d6af0d114822934
dndinidn <andiniputriamrullah@gmail.com>
2023-11-11 09:41:28 +0700

    array dan perulangan

---
+++ Day33.java
@@ -0,0 +1,21 @@
+package day;
+
+import java.util.Scanner;
+
+public class Day33 {
+
+    public static void main(String[] args) {
+        int[] array = new int[4];
+        Scanner dn = new Scanner(System.in);
+
+        for (int i = 0; i < array.length; i++) {
+            System.out.print("masukkan elemen ke- " + (i + 1) + " = ");
+            array[i] = dn.nextInt();
+        }
+        for (int t : array) {
+            System.out.print(t + " ");
+
+        }
+        System.out.println();
+    }
+}
