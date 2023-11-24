8f49b9ee65ef55327650a8cd829813f002bd87c1
dndinidn <andiniputriamrullah>
2023-11-24 12:17:38 +0800

    perulangan for each

---
+++ Day45.java
@@ -0,0 +1,16 @@
+package Day;
+
+import java.util.Arrays;
+
+public class Day45 {
+
+    public static void main(String[] args) {
+        int array[] = {2, 5, 6, 8, 2};
+        for (int angka : array) {
+            System.out.println(angka);
+
+        }
+
+    }
+
+}
