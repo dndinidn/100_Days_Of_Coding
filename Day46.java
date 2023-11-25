dd6536ad0dcf7397e50cfd5edca8d74957f49375
dndinidn <andiniputriamrullah>
2023-11-25 20:01:23 +0800

    array

---
+++ Day46.java
@@ -0,0 +1,16 @@
+package Day;
+
+public class Day46 {
+
+    public static void main(String[] args) {
+
+        String array[] = {"apel", "anggur", "semangka", "melon", "mangga"};
+        System.out.println("total elemen yang yang ada= " + array.length);
+        for (int i = 0; i < array.length; i++) {
+
+            System.out.println("indeks ke" + i + "= " + array[i]);
+
+        }
+
+    }
+}
