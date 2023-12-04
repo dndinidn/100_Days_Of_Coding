c4d9374de95713ad2033a0b4185f1d4f825498fc
dndinidn <andiniputriamrullah@gmail.com>
2023-12-04 17:04:37 +0800

    tipe data

---
+++ Day54.java
@@ -0,0 +1,15 @@
+package Day;
+
+import java.util.Scanner;
+
+public class Day54 {
+
+    public static void main(String[] args) {
+        Scanner dn = new Scanner(System.in);
+        System.out.println("masukkan ukuran baju= ");
+        char ukuranBaju = dn.next().charAt(0);
+        System.out.println("ukuran baju anda adalah " + ukuranBaju);
+
+    }
+
+}
