e5b22c52d73a3677a6fd059606a124269dc195ba
dndinidn <andiniputriamrullah@gmail.com>
2023-12-06 09:03:49 +0800

    Array

---
+++ Day59.java
@@ -0,0 +1,12 @@
+package Day;
+
+import java.util.Scanner;
+
+public class Day59 {
+
+    public static void main(String[] args) {
+        int[][] array = {{2, 3, 4}, {1, 2, 3}};
+        System.out.println(array[1][1]);
+    }
+
+}
