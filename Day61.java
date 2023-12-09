6bdad2f3913a717db3696ef02a052257b4c1b8a3
dndinidn <andiniputriamrullahgmail.com>
2023-12-08 16:17:06 -0800

    array

---
+++ Day61.java
@@ -0,0 +1,14 @@
+package day;
+
+public class Day61 {
+
+    public static void main(String[] args) {
+        String[] nama = {"harry", "hermione", "ron", "draco"};
+        for (int i = 0; i <= nama.length; i++) {
+            System.out.println("nama: " + nama[i]);
+
+        }
+
+    }
+
+}
