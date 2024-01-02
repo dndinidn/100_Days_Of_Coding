5b25399e4a7b7c5fb795defc8bca72f579cc9536
dndinidn <andiniputriamrullah@gmail.com>
2024-01-02 11:31:59 -1200

    perkalian 10 sampai 20

---
+++ Day86.java
@@ -0,0 +1,17 @@
+package Day;
+
+public class Day86 {
+
+    public static void main(String[] args) {
+        System.out.println("perkalian 10 sampai 20\n");
+        for (int i = 1; i <= 20; i++) {
+            for (int j = 10; j <= 20; j++) {
+                System.out.print(i * j + " ");
+
+            }
+            System.out.println("");
+
+        }
+    }
+
+}
