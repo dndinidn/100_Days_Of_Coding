fca07b6b9a2e6035618451074bc921626005bf05
dndinidn <andiniputriamrullah@gmail.com>
2024-01-13 19:11:09 -1200

    format String

---
+++ Day97.java
@@ -0,0 +1,11 @@
+
+package Day;
+
+public class Day97 {
+    public static void main(String[] args) {
+     String unsur ="neon";
+     int periode = 2;
+        System.out.printf("%1$s unsur denga periode 3 dan 1$s unsur yang berada pada golongan VIIIA",unsur);
+    }
+    
+}
