a919f7e76171c8316393c1e794af1ef8d99ae6ba
dndinidn <andiniputriamrullah@gmail.com>
2024-01-13 19:10:14 -1200

    format String

---
+++ Day96.java
@@ -0,0 +1,11 @@
+package Day;
+
+public class Day96 {
+
+    public static void main(String[] args) {
+        String unsur = "carbon";
+        int nomorAtom = 6;
+        System.out.printf("nma unsur : %s, nomor atom : %d", unsur, nomorAtom);
+    }
+
+}
