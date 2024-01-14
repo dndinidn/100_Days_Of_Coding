8a7c17000c1197dd369d07192d258aed2c18627e
dndinidn <andiniputriamrullah@gmail.com>
2024-01-13 19:09:19 -1200

    operator instanceof

---
+++ Day95.java
@@ -0,0 +1,11 @@
+package Day;
+
+public class Day95 {
+
+    public static void main(String[] args) {
+        String nama = "dn";
+        Day95 objek1 = new Day95();
+        System.out.println(objek1 instanceof Day95);
+    }
+
+}
